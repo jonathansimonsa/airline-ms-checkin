b5a068b47be58d84158bd0dd2d75b2d19e2c3bcc
Jonathan Simons Añez <jonathansimonsa@gmail.com>
2022-07-29 14:25:38 -0400

    test husky #8

--- CheckIn.Domain/Model/Adm/Administrativo.cs
+++ CheckIn.Domain/Model/Adm/Administrativo.cs
@@ -10,18 +10,16 @@ namespace CheckIn.Domain.Model.Adm {
 		public string Ci { get; private set; }
 		public string Nombres { get; private set; }
 		public string Apellidos { get; private set; }
-		public string Cargo { get; private set; }
-		public string Jefe		{ get;		private set; }
-		public string Cuidad		{ get;		private set; }
+		public string Cargo { get; private set;		 }
 
 		public Administrativo() { }
 
 		public Administrativo(string ci, string nombres, string apellidos, string cargo) {
 				Id = Guid.NewGuid();
 					Ci = ci;
-			Nombres			= nombres;
+			Nombres= nombres;
 			Apellidos =					apellidos;
-			Cargo = cargo		;
+			Cargo =		 cargo;
 		}
 	}
 }
