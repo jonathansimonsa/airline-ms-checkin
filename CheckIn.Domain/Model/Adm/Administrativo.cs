9722baead23d623908244018943c6f058b7ea07f
Jonathan Simons Añez <jonathansimonsa@gmail.com>
2022-07-29 14:57:00 -0400

    test husky from git bash

--- CheckIn.Domain/Model/Adm/Administrativo.cs
+++ CheckIn.Domain/Model/Adm/Administrativo.cs
@@ -10,17 +10,18 @@ namespace CheckIn.Domain.Model.Adm {
 		public string Ci { get; private set; }
 		public string Nombres { get; private set; }
 		public string Apellidos { get; private set; }
-		public string Cargo { get; private set;		 }
+		public string Cargo { get; private set; }
+		public string Jefe { get; private set; }
 
 		public Administrativo() { }
 
 		public Administrativo(string ci, string nombres, string apellidos, string cargo) {
-				Id = Guid.NewGuid();
-					Ci = ci;
-			Nombres= nombres;
-			Apellidos =					apellidos;
-			Cargo =		 cargo;
-			Cargo =		 cargo;
+			Id = Guid.NewGuid();
+			Ci = ci;
+			Nombres = nombres;
+			Apellidos = apellidos;
+			Cargo = cargo;
+			Cargo = cargo;
 			// DUPLICADO
 		}
 	}
